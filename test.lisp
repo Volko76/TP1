25ed47690844a03b5a992c4c1ee7f4ca2565621d
Volko76 <70014984+Volko76@users.noreply.github.com>
2023-09-21 18:23:22 +0200

    gtr

--- test.lisp
+++ test.lisp
@@ -1,11 +1,14 @@
-(defun factorial (n)
-  (if (<= n 1)
-      1
-      (* n (factorial (- n 1)))))
+(defun reverseA (arg1 arg2 arg3)
+    (format t "~a  ~a ~a" arg3 arg2 arg1))
+  
+(defun reverseB (&rest args)
+    (reverse args))
 
-(defun main ()
-  (format t "Enter a number: ")
-  (let ((input (read)))
-    (format t "The factorial of ~d is ~d.~%" input (factorial input))))
+(defun reverseC (&rest args)
+    (reverse args))
 
-(main)
+(defun double (L)
+    (loop for i from 1 to 15 do     ;On fait un boucle qui fait monter la variable i de 1 à 15
+        (if (atom (nth i L))        ;On regarde si l'objet à l'index i est un atom
+            ()                      ;Si oui
+            ())))                   ;Si non
