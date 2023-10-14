efc287232efc59fdece65c1fd840cddd61affd15
mehd-i <147080882+mehd-i@users.noreply.github.com>
2023-10-14 20:10:09 +0200

    Update ex3.lisp

--- ex3.lisp
+++ ex3.lisp
@@ -11,11 +11,11 @@
 ;(my-assoc 'Pierre '((Yolande 25) (Pierre 22) (Julie 45)))
 ; Résultat: (Pierre 22)
 
-(my-assoc 'Yves '((Yolande 25) (Pierre 22) (Julie 45)))
+;(my-assoc 'Yves '((Yolande 25) (Pierre 22) (Julie 45)))
 ; Résultat: NIL
 
-(cles '((Yolande 25) (Pierre 22) (Julie 45)))
+;(cles '((Yolande 25) (Pierre 22) (Julie 45)))
 ; Résultat: (Yolande Pierre Julie)
 
-(creation '(Yolande Pierre Julie) '(25 22 45))
+;(creation '(Yolande Pierre Julie) '(25 22 45))
 ; Résultat: ((Yolande 25) (Pierre 22) (Julie 45))
